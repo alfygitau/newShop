4bc0e7629c6b08e15a7a244478379c5a125ea50a
alfygitau <alfygitau@gmail.com>
2022-06-27 17:43:52 +0300

    added react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,24 @@ import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import Homepage from "./pages/Homepage";
 
+// router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetails from "./pages/ProductDetails";
+
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <Homepage />
+          <Routes>
+            <Route path="" element={<Homepage />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
