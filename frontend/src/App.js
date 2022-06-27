351c9446ad4e5513adb31ab85f4a9da3c69138c7
alfygitau <alfygitau@gmail.com>
2022-06-27 15:34:15 +0300

    fixed the navbar and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
+// external modules
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to DeliverX shop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to DeliverX shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
