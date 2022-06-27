d403a66dc83d70e3eb7f1ad762c86ca7f2ccd93d
alfygitau <alfygitau@gmail.com>
2022-06-27 23:34:05 +0300

    added a connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
